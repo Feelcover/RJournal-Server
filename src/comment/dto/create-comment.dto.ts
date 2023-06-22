1513609d77f6ac1c7af32960c28f0d63391f840c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-22 23:29:45 +0300

    dto comment

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,6 +1,9 @@
-import { IsString } from "class-validator";
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCommentDto {
-    @IsString()
-    text: string;
+  @IsString()
+  text: string;
+
+  @IsNumber()
+  postId: number;
 }
