488204c6f804ca339112fc169aa54c09bf5edb15
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-24 14:59:53 +0300

    validateUser и подключение в модуле

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-
+import { UserService } from 'src/user/user.service';
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private userService: UserService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    if (user && user.password === password) {
+      const { password, ...result } = user;
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+      return result;
+    }
   }
 }
