826cc5c11397a28cf73ef3854fc897579c8bfc25
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-25 22:03:33 +0300

    подключение JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,18 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret-key',
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
