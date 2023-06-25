3855c111ef4dc6b32f1b05f8b89d50554193aafd
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-25 22:36:53 +0300

    подключение jwtstrategy в модуль auth и ренейм файла LocalAuthGuard

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: false, // игнорируем невалидный токен
       secretOrKey: 'secret-key',
     });
   }
