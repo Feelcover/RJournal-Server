9bdb78107d8dcef6d5cb207d77141327493c30a2
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-27 16:23:05 +0300

    подключение регистрации в контроллере auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,15 @@ import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local.guard';
 import { JwtAuthGuard } from './guards/jwt.guard';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -17,5 +22,10 @@ export class AuthController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Post('register')
+  register(createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 }
